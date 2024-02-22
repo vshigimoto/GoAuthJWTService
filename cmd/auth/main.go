d0a8d199704761582417614b50ab5ba33e70ee90
Amirkhan <shigimoto@mail.ru>
2024-02-23 03:05:04 +0600

    add main functional

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"go.uber.org/zap"
 
+	"github.com/vshigimoto/GoAuthJWTService/internal/applicator"
+	"github.com/vshigimoto/GoAuthJWTService/internal/config"
+)
+
+func main() {
+	logger, _ := zap.NewProduction()
+	defer func(logger *zap.Logger) {
+		err := logger.Sync()
+		if err != nil {
+			return
+		}
+	}(logger)
+
+	l := logger.Sugar()
+	l = l.With(zap.String("app", "medods"))
+
+	var cfg config.Config
+	err := cleanenv.ReadConfig("config/auth.yaml", &cfg)
+	if err != nil {
+		l.Fatalf("Failure read config err: %v", err)
+		return
+	}
+
+	app := applicator.New(l, &cfg)
+	app.Run()
 }
